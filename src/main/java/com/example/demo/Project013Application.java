3e061e8a270848e468eff1620eeae0adb2202fe8
JuYoung <jjystar3@gmail.com>
2024-10-10 14:56:00 +0900

    first commit

---
+++ src/main/java/com/example/demo/Project013Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project013Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project013Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Project013ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project013ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
